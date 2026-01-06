3cdb678c8be0e663866228a4c7c0e95bd1bc24b6
Stephen882-pixel <stephenondeyo0@gmail.com>
2026-01-06 13:24:07 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/Stephen882-pixel/Amazon-Clone-With-ReactJS",
   plugins: [
     react({
       babel: {
