70695e05c1f3e71a16234ff7fd59df5a1df1b87d
Stephen882-pixel <stephenondeyo0@gmail.com>
2026-01-06 17:28:15 +0300

    product section

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import './Home.css'
-
+import Product from './Product'
 function Home() {
   return (
     <>
@@ -9,7 +9,15 @@ function Home() {
      <Header />
    
     <div className='home'>
-      <img  className='home__image' src="/images/Hero.jpg" alt="" />      
+      <img  className='home__image' src="/images/Hero.jpg" alt="" />   
+
+      <Product 
+        id="12321341"
+        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+        price={29.99}
+        rating={5}
+        image="/images/Drone.jpg"
+      />   
     </div>
 
      </>
